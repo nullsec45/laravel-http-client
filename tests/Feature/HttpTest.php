e51fb566c5de10641ec8d1f31b5c70af1e15ca1a
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-08-03 10:50:30 +0700

    test : test get. post, delete, response

---
+++ tests/Feature/HttpTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Illuminate\Support\Facades\Http;
+
+class HttpTest extends TestCase
+{
+    public function testGet(){
+        $response=Http::get("https://eokih5piy7jx0o1.m.pipedream.net/");
+        self::assertTrue($response->ok());
+    }
+
+    public function testPost(){
+        $response=Http::post("https://eokih5piy7jx0o1.m.pipedream.net/");
+        self::assertTrue($response->ok());
+    }
+
+    public function testDelete(){
+        $response=Http::delete("https://eokih5piy7jx0o1.m.pipedream.net/");
+        self::assertTrue($response->ok());
+    }
+
+    public function testReponse(){
+        $response=Http::get("https://eokih5piy7jx0o1.m.pipedream.net/");
+        self::assertEquals(200, $response->status());
+        self::assertNotNull($response->headers());
+        self::assertNotNull($response->body());
+
+        $json=$response->json();
+        self::assertNotNull($json["about"]);
+    }
+}
