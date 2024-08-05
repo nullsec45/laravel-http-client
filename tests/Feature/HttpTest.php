7867bce861203bb61406e1003d6405b7a1b85fec
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-08-05 23:24:01 +0700

    test : test query parameter, header, cookie, form post, multipart, json

--- tests/Feature/HttpTest.php
+++ tests/Feature/HttpTest.php
@@ -33,4 +33,68 @@ class HttpTest extends TestCase
         $json=$response->json();
         self::assertNotNull($json["about"]);
     }
+
+    public function testQueryParameter(){
+        $response=Http::withQueryParameters([
+            "page" => 1,
+            "limit" => 10
+        ])->get("https://eokih5piy7jx0o1.m.pipedream.net/");
+        self::assertTrue($response->ok());
+    }
+
+    public function testHeader(){
+        $response=Http::withQueryParameters([
+            "page" => 1,
+            "limit" => 10
+        ])->withHeaders([
+            "Accept" => "application/json",
+            "X-Request-Id" => "123456789"
+        ])->get("https://eokih5piy7jx0o1.m.pipedream.net/");
+
+        self::assertTrue($response->ok());
+    }
+
+    public function testCookie(){
+        $response=Http::withQueryParameters([
+            "page" => 1,
+            "limit" => 10
+        ])->withHeaders([
+            "Accept" => "application/json",
+            "X-Request-Id" => "123456789"
+        ])->withCookies([
+            "sessionId" => "1234567890",
+            "userId" => "1  "
+        ],"https://eokih5piy7jx0o1.m.pipedream.net/")->get("https://eokih5piy7jx0o1.m.pipedream.net/");
+
+        self::assertTrue($response->ok());
+    }
+
+    public function testFormPost(){
+        $reponse=Http::asForm()->post("https://eokih5piy7jx0o1.m.pipedream.net/",[
+            "username" => "admin",
+            "password" => "123456"
+        ]);
+
+        self::assertTrue($reponse->ok());    
+    }
+
+    public function testMultiPart(){
+        $reponse=Http::asMultiPart()->attach("typescript", file_get_contents("https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/2048px-Typescript_logo_2020.svg.png"), "typescript.png")
+                                    ->post("https://eokih5piy7jx0o1.m.pipedream.net",[
+                                        "username" => "admin",
+                                        "password" => "123456"
+                                    ]);
+        
+        self::assertTrue($reponse->ok());
+    }
+
+    public function testJSON(){
+        $response=Http::asJson()->post("https://eokih5piy7jx0o1.m.pipedream.net",[
+            "username" => "admin",
+            "password" => "12345"
+        ]);
+
+        self::assertTrue($response->ok());
+
+    }
 }
